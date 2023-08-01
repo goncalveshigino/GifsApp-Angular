d5b8bd73b3adea9be4a7806a63e19a024f30ca85
goncalveshigino <goncalveshigino20@gmail.com>
2023-08-01 06:18:22 +0100

    GifsApp first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GifsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,14 @@
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  templateUrl: 'search-box.component.html'
+})
+
+export class SearchBoxComponent  {
+  constructor() { }
+
+}
+
